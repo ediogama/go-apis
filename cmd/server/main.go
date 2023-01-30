a91e3a9ca61b9bb5bfe56d1b662ecbec3b76ed47
Edio Gama <ediogama@hotmail.com>
2023-01-30 19:40:49 -0300

    add go-chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ediogama/go-apis/internal/entity"
 	"github.com/ediogama/go-apis/internal/infra/database"
 	"github.com/ediogama/go-apis/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,7 +27,10 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	http.ListenAndServe(":8000", nil)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
